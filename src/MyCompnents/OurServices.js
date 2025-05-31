13e49ad54b9ae0b8ff9b1c258ed9116b28b11a89
Yug Tamang <yugtamang@example.com>
2025-05-31 08:28:18 +0545

    Our Service Page

---
+++ src/MyCompnents/OurServices.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Services from './services/ServiceList';
+
+export default function OurServices() {
+  
+  return (
+    <div> 
+
+      <Services/>
+      
+    </div>
+  );
+}
